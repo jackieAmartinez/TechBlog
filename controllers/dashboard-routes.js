409ec8659d1b9916355ef3b303c4fd9e5c1a0eb2
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-27 14:24:44 -0400

    switch declarations and user-home get route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,25 +1,44 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
-const withAuth = require('../utils/auth');
+const { User, Blogpost, Comment } = require("../models");
+const authorize = require("../utils/authorize");
 
-router.get('/', withAuth, async (req, res) => {
+router.get("/user-home", authorize, async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      where: {
-        userId: req.session.userId,
-      },
+    // Find the logged in user based on the session ID and return their shelf data
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blogpost }],
     });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render('all-posts-admin', {
-      layout: 'dashboard',
-      posts,
+    const user = userData.get({ plain: true });
+    res.render("user-home", {
+      layout: "dashboard",
+      ...user,
+      logged_in: true,
     });
+    // tells what error occurred
   } catch (err) {
-    res.redirect('login');
+    res.status(500).json(err);
   }
 });
+
+// router.get('/', withAuth, async (req, res) => {
+//   try {
+//     const postData = await Post.findAll({
+//       where: {
+//         userId: req.session.userId,
+//       },
+//     });
+
+//     const posts = postData.map((post) => post.get({ plain: true }));
+
+//     res.render('all-posts-admin', {
+//       layout: 'dashboard',
+//       posts,
+//     });
+//   } catch (err) {
+//     res.redirect('login');
+//   }
+// });
 
 router.get('/new', withAuth, (req, res) => {
   res.render('new-post', {
