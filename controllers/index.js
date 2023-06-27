f7d054dc182c3c3972f83798f5b8832cde1068d6
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-27 13:37:13 -0400

    model variants changed in post request

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,22 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
-const homeRoutes = require("./homeroutes.js");
-const dashboardRoutes = require("./dashboard-routes.js");
+// const router = require("express").Router();
+// const apiRoutes = require("./api");
+// const homeRoutes = require("./homeroutes.js");
+// const dashboardRoutes = require("./dashboard-routes.js");
 
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-router.use("/dashboard", dashboardRoutes);
-
-module.exports = router;
-
-// const router = require('express').Router();
-
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes');
-// // const dashRoutes = require('./dashboard-routes');
-
-// router.use('/', homeRoutes);
-// // router.use('/dashboard', dashRoutes);
-// router.use('/api', apiRoutes);
+// router.use("/", homeRoutes);
+// router.use("/api", apiRoutes);
+// router.use("/dashboard", dashboardRoutes);
 
 // module.exports = router;
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dashboard-routes');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
