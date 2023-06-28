467705b8167573b9a6a83f781494731cec23df13
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-27 20:10:11 -0400

    reference model switch for postID

--- models/comment.js
+++ models/comment.js
@@ -1,62 +1,43 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+// required dependencies
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
+// game model to organize the shelf before table gets flipped
 class Comment extends Model {}
 
 Comment.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     body: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
+    userID: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    postID: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "blogpost",
+        key: "id",
+      },
+    },
   },
   {
-    sequelize
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
   }
 );
 
 module.exports = Comment;
-
-// // required dependencies
-// const { Model, DataTypes } = require("sequelize");
-// const sequelize = require("../config/connection");
-
-// // game model to organize the shelf before table gets flipped
-// class Comment extends Model {}
-
-// Comment.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     body: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     userID: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "user",
-//         key: "id",
-//       },
-//     },
-//     postID: {
-//       type: DataTypes.INTEGER,
-//       references: {
-//         model: "blogpost",
-//         key: "id",
-//       },
-//     },
-//   },
-//   {
-//     sequelize,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: "comment",
-//   }
-// );
-
-// module.exports = Comment;
