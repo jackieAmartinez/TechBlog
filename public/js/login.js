3349e3500792023257d0450ac8928799813bbf36
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-27 20:24:39 -0400

    local variable names

--- public/js/login.js
+++ public/js/login.js
@@ -2,49 +2,26 @@
 const loginUser = async (event) => {
     event.preventDefault();
     // local variables
-    const username = document.querySelector("#submit-username").value.trim();
-    const password = document.querySelector("#submit-password").value.trim();
-    // got a light, check for matches
+    const username = document.querySelector("#username-login-input").value.trim();
+    const password = document.querySelector("#password-login-input").value.trim();
+    // verify
     if (username && password) {
       const response = await fetch("/api/users/login", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
       });
-      // good match show shelf, bad match yell at user
+      // display or error
       if (response.ok) {
-        document.location.replace("/shelf");
+        document.location.replace("/dashboard/user-home");
       } else {
         alert(response.statusText);
       }
     }
   };
   
-  // function for new user
-  const createUser = async (event) => {
-    event.preventDefault();
-    // local variables
-    const username = document.querySelector("#create-username").value.trim();
-    const email = document.querySelector("#create-email").value.trim();
-    const password = document.querySelector("#create-password").value.trim();
-    // make sure new user not a clone
-    if (username && email && password) {
-      const response = await fetch("/api/users/", {
-        method: "POST",
-        body: JSON.stringify({ username, email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
-      // user approved allow shelf view, disapproved user gets yelled at
-      if (response.ok) {
-        document.location.replace("/shelf");
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  
   
   // review view port to find appropriate locations to listen in order to execute functions
   document.querySelector("#login-form").addEventListener("submit", loginUser);
-  
-  document.querySelector("#signup-form").addEventListener("submit", createUser);
   
